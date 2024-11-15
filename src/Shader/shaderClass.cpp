c77cc81326d22e513f0b4d0b8eb22d7f489777e7
Missing Textures <dariormt@gmail.com>
2024-11-15 19:12:22 +0100

    Changed fallback shaders declaration

--- src/Shader/shaderClass.cpp
+++ src/Shader/shaderClass.cpp
@@ -7,20 +7,23 @@
 #include <cstring>
 
 // Missing Vertex Shader source code
-const char* missingVertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+const char* missingVertexShaderSource = R"glsl(
+#version 460 core
+layout (location = 0) in vec3 aPos;
+void main()
+{
+   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)glsl";
 
 // Missing Fragment Shader source code
-const char* missingFragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.5f, 0.0f, 0.5f, 1.0f);\n"
-"}\n\0";
+const char* missingFragmentShaderSource = R"glsl(
+#version 460 core
+out vec4 FragColor;
+void main()
+{
+   FragColor = vec4(0.5f, 0.0f, 0.5f, 1.0f);
+})glsl";
 
 /**
  * \brief Reads the contents of a file and returns it as a string.
